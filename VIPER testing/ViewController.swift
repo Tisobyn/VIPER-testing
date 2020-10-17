1b2807e1e1cd70edeb97474dd0e496f8db666d95
Yermek Sabyrzhan <Tisobyn@users.noreply.github.com>
2020-10-17 18:46:00 +0600

    Initial Commit

---
+++ VIPER testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER testing
+//
+//  Created by Yermek Sabyrzhan on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
